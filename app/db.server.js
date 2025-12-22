05b02fd00f9170cfc7c487b93e3bbc65ba8f6c8b
Digittrix Dev <dev@digittrix.in>
2025-12-22 15:55:38 +0530

    update

--- app/db.server.js
+++ app/db.server.js
@@ -1,14 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-let db;
-
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-} else {
+if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
     global.prismaGlobal = new PrismaClient();
   }
-  db = global.prismaGlobal;
 }
 
-export { db };
+const prisma = global.prismaGlobal ?? new PrismaClient();
+
+export default prisma;
